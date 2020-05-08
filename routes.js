f95f90671a27f41bb1a54e28add73dd5751664c3
henriqueFutema <henrique.futema01@gmail.com>
2020-05-07 23:20:50 -0300

    Procfile

--- routes.js
+++ routes.js
@@ -6,10 +6,10 @@ const UserController = require("./controllers/userController");
 
 const auth = require("./middlewares/auth");
 
-routes.get("/posts", auth, PostController.index);
-routes.post("/posts", auth, PostController.store);
-routes.delete("/posts/:id", auth, PostController.destroy);
-routes.put("/posts/:id", auth, PostController.update);
+routes.get("/posts", PostController.index);
+routes.post("/posts", PostController.store);
+routes.delete("/posts/:id", PostController.destroy);
+routes.put("/posts/:id", PostController.update);
 
 routes.post("/users", UserController.store);
 routes.post("/auth", UserController.signIn);
